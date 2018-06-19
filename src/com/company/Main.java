c2d4e9c5419c3d3b3d3a3d18d5053dc83b385309
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-19 15:54:12 +0200

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Bank bank = new Bank("Raiffeisen");
+        bank.addNewBranch("nbgd");
+        bank.addBranchCustomer("nbgd","kole",20);
+        bank.addBranchCustomer("nbgd","sale",10);
+        bank.printBranchCustomers("nbgd",true);
 
     }
 }
